8eebabe3d4fd6cb5845b9940597bda4ddfd7e86d
mcotton <tifosigoon@gmail.com>
2020-09-25 13:57:39 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# EE-streaming #
+Quick example project for using the Eagle Eye Networks API.  The goal is to show how a live camera stream can be embedded into a webpage.  This program handles logging into the API, and returns the `camera_id` and `auth_key`.
+
+## Installation ##
+`docker-compose up --build -d`
+
+or you can run it locally on python3.
+
+Make sure you have valid credentials in  `local_settings.py`
+
+
+## Usage ##
+TODO:
+
+
+## Troubleshooting ##
+TODO:
+
---
+++ server.py
@@ -0,0 +1,69 @@
+
+import local_settings
+
+from EagleEye import EagleEye
+een = EagleEye()
+
+from twisted.web import server, resource
+from twisted.internet import reactor, endpoints, task
+
+import json
+
+
+output = { 
+            "cameras": None,
+            "auth_key": None
+        }
+
+
+timeout = 300
+
+class Counter(resource.Resource):
+    isLeaf = True
+
+    def render_GET(self, request):
+        request.setHeader(b"content-type", b"application/json")
+        request.setHeader('Access-Control-Allow-Origin', '*')
+        request.setHeader('Access-Control-Allow-Methods', 'GET')
+        request.setHeader('Access-Control-Allow-Headers', 'x-prototype-version,x-requested-with')
+        request.setHeader('Access-Control-Max-Age', '2520') # 42 hours
+        
+        content = json.dumps(output)
+        return content.encode("ascii")
+
+
+
+def login_to_EagleEye(een):
+    print("login_to_EagleEye called")
+    login = een.login(local_settings.username, local_settings.password)
+    if login:
+        print(f"login succeeded, updating devices and cookie {een.session.cookies['auth_key']}")
+        output['auth_key'] = een.session.cookies['auth_key']
+        output['cameras'] = [{"id": i.camera_id, "name": i.name} for i in een.cameras] 
+    else:
+        print("login_to_EagleEye failed")
+
+
+
+def check_cookie():
+    #check if auth_key is valid
+    if output['auth_key']:
+        response = een.session.request("GET", "https://login.eagleeyenetworks.com/g/aaa/isauth")
+        if response.status_code == 200:
+            print(f"cookie is still good {een.session.cookies['auth_key']}")
+            output['auth_key'] = een.session.cookies['auth_key']
+        else:
+            print(f"isauth check failed with {response.status_code}")
+            login_to_EagleEye(een)
+    else:
+        print(f"auth_key is set to {output['auth_key']}")
+        login_to_EagleEye(een)
+
+
+
+
+l = task.LoopingCall(check_cookie)
+l.start(timeout) # call every sixty seconds
+
+endpoints.serverFromString(reactor, "tcp:8080").listen(server.Site(Counter()))
+reactor.run()
